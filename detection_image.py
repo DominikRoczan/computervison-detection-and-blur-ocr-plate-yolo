6d9a11a8f74874fe2a83ce3e6c049042e7d3e32e
Dominik Roczan <70064951+DominikRoczan@users.noreply.github.com>
2024-06-12 10:53:40 +0200

    update

--- detection_image.py
+++ detection_image.py
@@ -7,45 +7,33 @@ import csv
 directory_path = '02_input_blur'
 
 # Lista na ścieżki do zdjęć
-image_paths = []
-
-# Sprawdź wszystkie pliki w katalogu
-for filename in os.listdir(directory_path):
-    # Sprawdź, czy plik jest plikiem graficznym (np. jpg, png)
-    if filename.endswith('.jpg') or filename.endswith('.png'):
-        # Jeśli tak, dodaj jego pełną ścieżkę do listy image_paths
-        image_paths.append(os.path.join(directory_path, filename))
+image_paths = [os.path.join(directory_path, f) for f in os.listdir(directory_path) if f.endswith('.jpg') or f.endswith('.png')]
 
 # Załaduj model YOLO
-model_path = 'trained_model/train23/weights/62_best.pt'
+model_path = 'trained_model/train23/weights/90_best.pt'
 model = YOLO(model_path)  # Używając ścieżki do wcześniej wytrenowanego modelu
 
-# Ścieżka do zapisania pliku tekstowego
-dir_name = '03_output_blur'
-if not os.path.exists(dir_name):
-    os.makedirs(dir_name, exist_ok=True)
+# Ścieżki do zapisania plików
+output_dirs = {
+    'blurred': '03_output_blur',
+    'cropped': '03_output_cropped',
+    'csv': '03_output_csv'
+}
 
-# Ścieżka do zapisania wyciętych obrazów
-cropped_dir = '03_output_cropped'
-if not os.path.exists(cropped_dir):
-    os.makedirs(cropped_dir, exist_ok=True)
-
-# Ścieżka do katalogu z plikami CSV
-csv_dir = '03_output_csv'
-if not os.path.exists(csv_dir):
-    os.makedirs(csv_dir, exist_ok=True)
+for dir_path in output_dirs.values():
+    os.makedirs(dir_path, exist_ok=True)
 
 # Ścieżka do pliku CSV
-csv_file_path = os.path.join(csv_dir, 'detections.csv')
+csv_file_path = os.path.join(output_dirs['csv'], 'detections.csv')
 
 # Sprawdzenie, czy plik CSV istnieje; jeśli nie, utwórz nowy plik CSV i zapisz nagłówki
 if not os.path.exists(csv_file_path):
     with open(csv_file_path, mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(['Nazwa zdjęcia', 'Detekcja', 'Score'])  # Nagłówki kolumn
+        writer.writerow(['Image name', 'Detection', 'Score'])  # Nagłówki kolumn
 
-# Słownik do śledzenia licznika dla każdego pliku
-count = {}
+# Słownik do śledzenia liczby detekcji i wyników dla każdego pliku
+results_dict = {}
 
 # Przetwarzaj wszystkie obrazy w katalogu
 for image_path in image_paths:
@@ -54,64 +42,49 @@ for image_path in image_paths:
     # Przeprowadź detekcję na obrazie
     results = model(image)
 
-    idx = 0
     detected = False  # Flaga, czy wykryto jakieś tablice
-    cropped_images = []  # Lista na wycięte obrazy tablic
+    confidence_scores = []  # Lista na wartości confidence
 
     # Przetwarzanie wyników detekcji
     for r in results:
-        boxes = r.boxes
-        class_names = list(r.names.values())
+        if hasattr(r, 'boxes'):
+            boxes = r.boxes
+            for b in boxes:
+                xyxy = b.xyxy.cpu().numpy().astype(int).flatten()
+                confidence = float(b.conf)
+                class_id = int(b.cls)
+                class_name = r.names[class_id]
+                label = f"{class_name} {confidence:.2f}"
 
-        # Iteruj przez każde pole (bounding box)
-        for b in boxes:
-            xyxy = b.xyxy  # Zakładamy, że xyxy to tensor z 4 wartościami [x1, y1, x2, y2]
+                if confidence > 0.15:
+                    detected = True
+                    confidence_scores.append(confidence)
 
-            confidence = float(b.conf)  # Prawidłowa pewność detekcji
-            class_id = b.cls.item()  # Get class ID for each box
-            class_name = r.names[class_id]  # Convert class ID to class name
-            label = f"{class_name} {confidence:.2f}"
+                    cropped_image = image[xyxy[1]:xyxy[3], xyxy[0]:xyxy[2]]
+                    cropped_name = f'{os.path.splitext(os.path.basename(image_path))[0]}_Cropped_{len(confidence_scores)}.jpg'
+                    cv2.imwrite(os.path.join(output_dirs['cropped'], cropped_name), cropped_image)
 
-            if confidence > 0.45:
-                detected = True
-                box_width = int(xyxy[0][2]) - int(xyxy[0][0])
-                # Inkrementuj licznik dla aktualnego pliku
-                count[image_path] = count.get(image_path, 0) + 1
-                current_count = count[image_path]
-                print(f'{current_count} Conf: {confidence:.2f}')
+                    cropped_region = image[xyxy[1]:xyxy[3], xyxy[0]:xyxy[2]]
+                    blurred_region = cv2.blur(cropped_region, (23, 23))
+                    image[xyxy[1]:xyxy[3], xyxy[0]:xyxy[2]] = blurred_region
 
-                # cropped plates
-                cropped_image = image[int(xyxy[0][1]):int(xyxy[0][3]), int(xyxy[0][0]):int(xyxy[0][2])]
-                cropped_images.append(cropped_image)
-
-                # Zapisz wycięty obraz do folderu input_cropped
-                cropped_name = f'{os.path.splitext(os.path.basename(image_path))[0]}_Cropped.jpg'
-                cv2.imwrite(os.path.join(cropped_dir, cropped_name), cropped_image)
-
-                # Zablurowanie obszaru wewnątrz prostokąta
-                x1, y1, x2, y2 = int(xyxy[0][0]), int(xyxy[0][1]), int(xyxy[0][2]), int(xyxy[0][3])
-                cropped_region = image[y1:y2, x1:x2]
-                blurred_region = cv2.blur(cropped_region, (23, 23))  # Rozmycie Gaussowskie
-
-                # Wstawienie zblurowanego obszaru z powrotem do obrazu
-                image[y1:y2, x1:x2] = blurred_region
-
+    # Zapisz zmodyfikowane zdjęcie do odpowiedniego folderu
     if detected:
-        # Zapisywanie obrazu końcowego
-        final_image_path = os.path.join(dir_name, f'{os.path.splitext(os.path.basename(image_path))[0]}_Blur.jpg')
+        final_image_path = os.path.join(output_dirs['blurred'], f'{os.path.splitext(os.path.basename(image_path))[0]}_Blur.jpg')
         cv2.imwrite(final_image_path, image)
-
     else:
-        # Obsługa przypadku, gdy nie wykryto tablic
+        final_image_path = os.path.join(output_dirs['blurred'], os.path.basename(image_path))
+        cv2.imwrite(final_image_path, image)
         print(f"No license plates were detected in {image_path}.")
 
+    # Zapisz wyniki detekcji w słowniku
+    results_dict[image_path] = confidence_scores if detected else 'None'
+
 # Zapisywanie informacji do pliku CSV
-with open(csv_file_path, mode='w', newline='') as file:
+with open(csv_file_path, mode='a', newline='') as file:
     writer = csv.writer(file)
     for image_path in image_paths:
-        filename = os.path.basename(image_path)  # Pobierz nazwę pliku bez ścieżki
-        # Pobierz liczbę wykrytych tablic lub zapisz "None", jeśli nie wykryto żadnych
-        detection_count = count.get(image_path, 'None')
-        # Pobierz wynik prawdopodobieństwa
-        score = "{:.2f}".format(float(count.get(image_path, 0))) if detection_count != 'None' else 'None'
-        writer.writerow([filename, detection_count, score])
+        filename = os.path.basename(image_path)
+        detection_count = len(results_dict[image_path]) if results_dict[image_path] != 'None' else 'None'
+        scores = ', '.join([f"{score:.2f}" for score in results_dict[image_path]]) if detection_count != 'None' else 'None'
+        writer.writerow([filename, detection_count, scores])

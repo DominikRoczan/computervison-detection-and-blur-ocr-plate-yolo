37b223a748f8ca3f7cbeaae6b2cab9707413bc3a
Dominik Roczan <70064951+DominikRoczan@users.noreply.github.com>
2024-08-06 16:28:38 +0200

    update

--- detection-platte.py
+++ detection-platte.py
@@ -1,25 +1,28 @@
-import os
+import torch
 import cv2
 import argparse
 import json
 from ultralytics import YOLO
+import warnings
 
-# Stałe ustawienia
-MODEL_PATH = 'trained_model/90_best.pt'  # Ścieżka do modelu YOLO
-OUTPUT_DIR = 'output_blurred'  # Katalog wynikowy dla zdjęć po przetworzeniu
+# Disable cudnn
+torch.backends.cudnn.enabled = False
+
+# Disable warnings
+warnings.filterwarnings("ignore")
+
+# Path to the YOLO model
+MODEL_PATH = 'trained_model/90_best.pt'
 
 def process_single_image(image_path):
     """
-    Process a single image, performing detection, saving the result, and storing detection coordinates in a JSON file.
+    Process a single image, performing detection, and displaying detection coordinates as JSON.
 
     Args:
     - image_path (str): Path to the image file.
     """
     # Load YOLO model
     model = YOLO(MODEL_PATH)  # Using the predefined path to the model
-
-    # Create output directories if they don't exist
-    os.makedirs(OUTPUT_DIR, exist_ok=True)
 
     # Load the image
     image = cv2.imread(image_path)
@@ -46,9 +49,6 @@ def process_single_image(image_path):
                 if confidence > 0.35:
                     detected = True
 
-                    # Draw red bounding box and put confidence score
-                    cv2.rectangle(image, (xyxy[0], xyxy[1]), (xyxy[2], xyxy[3]), (0, 0, 255), 2)
-
                     # Store detection coordinates in a dictionary
                     conf = f'{confidence: .2f}'
                     detection_info = {
@@ -61,43 +61,16 @@ def process_single_image(image_path):
                     }
                     detections.append(detection_info)
 
-                    # Print coordinates of the bounding box
-                    #print(f"Class: {class_name}, Confidence: {confidence:.2f}")
-                    #print(f"Bounding Box Coordinates: A-({xyxy[0]}, {xyxy[1]}), B-({xyxy[2]}, {xyxy[3]})")
-
-    # Save detection coordinates to JSON file
-    json_output_path = os.path.join(OUTPUT_DIR, f'{os.path.splitext(os.path.basename(image_path))[0]}.json')
-    with open(json_output_path, 'w') as json_file:
-        json.dump(detections, json_file, indent=4)
-
-    # Save modified image to the appropriate folder
-    if detected:
-        final_image_path = os.path.join(OUTPUT_DIR, f'{os.path.splitext(os.path.basename(image_path))[0]}_Blur.jpg')
-        cv2.imwrite(final_image_path, image)
-    else:
-        final_image_path = os.path.join(OUTPUT_DIR, os.path.basename(image_path))
-        cv2.imwrite(final_image_path, image)
-        print(f"No license plates were detected in {image_path}.")
-
-    # Display the processed image
-    cv2.imshow('Processed Image', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    # Open and print the contents of the JSON file
-    if os.path.exists(json_output_path):
-        with open(json_output_path, 'r') as json_file:
-            json_data = json.load(json_file)
-            print("Contents of JSON file:")
-            print(json.dumps(json_data, indent=4))
+    # Display the detection coordinates as JSON
+    print("Contents of JSON file:")
+    print(json.dumps(detections, indent=4))
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Process a single image with YOLO model, display the result, and save detection coordinates in JSON.')
+    parser = argparse.ArgumentParser(description='Process a single image with YOLO model and display detection coordinates as JSON.')
     parser.add_argument('image_path', type=str, help='Path to the image file')
 
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_arguments()
-
     process_single_image(args.image_path)
